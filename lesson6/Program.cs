b696f54b0cf575ea13c73978ba9b64a9c11796aa
kasuigetsu <arina_barsukova@bk.ru>
2022-02-21 17:09:59 +0300

    Добавьте файлы проекта.

---
+++ lesson6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] process = Process.GetProcesses();
+            Console.WriteLine("ID\t\tName");
+            for (int i = 0; i < process.Length; i++)
+            {
+                Console.WriteLine($"{process[i].Id}\t\t{process[i].ProcessName}");
+            }
+            try
+            {
+            Console.Write("\nВведите ID процесса, который желаете завершить: ");
+            int processID = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < process.Length; i++)
+                {
+                    if(processID==process[i].Id)
+                    {
+                        process[i].Kill();
+                    }
+                }
+            }
+            catch(FormatException exception)
+            {
+                Console.WriteLine($"Ошибка: {exception.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
